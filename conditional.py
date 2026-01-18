46e067a07d6950ebdd99d893968d74705a11a386
Rohit Chandra <rohitchandra@Mac.lan>
2026-01-18 18:31:26 +0530

    Cleanup duplicate files

--- conditional.py
+++
@@ -1,6 +0,0 @@
-# Check if 5 is greater than 2
-if 5 > 2:
-    # If the condition checks out, print this message to the console
-    print ("5 is greater than 2")
-# Print this message to the console regardless of the condition above
-print("5 is greater than 2")
--- name.py
+++
@@ -1,6 +0,0 @@
-# Prompt the user to enter their name and store the input in variable A
-A = input("Entr your Name: ")
-# Print the string "the name is " followed by the value stored in variable A
-print("the name is ", A)
-# A personal note indicating this is the first day of learning Python
-#This is my 1st day of Python
