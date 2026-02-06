5bcb91a8fa365f4bd58f2ebd75627d55bbb84468
Rohit Chandra <rohitchandra@Rohits-MacBook-Pro.local>
2026-02-06 15:09:34 +0530

    Add comments to linkedlist.c

--- C-Programming/DSA SEM 2/linkedlist.c
+++ C-Programming/DSA SEM 2/linkedlist.c
@@ -1,57 +1,103 @@
-// Standard input/output library for printf and scanf functions
 #include <stdio.h>
-
-// Standard library for memory allocation functions like malloc
 #include <stdlib.h>
 
-// Structure definition for a singly linked list node
-struct slinklist {
-  int data;               // Data field to store integer value
-  struct slinklist *next; // Pointer to the next node in the list
+// Structure definition for the linked list node
+// Note: Named 'dlinklist' but structure implies singly linked list (only 'next'
+// pointer)
+struct dlinklist {
+  int data;               // Data of the node
+  struct dlinklist *next; // Pointer to the next node
 };
 
-// Global pointer to the start/head of the linked list
-// Initialized to NULL indicating an empty list
-struct slinklist *start = NULL;
+// Global pointer to the start of the list
+struct dlinklist *start = NULL;
 
-// Function to create and return a new node
-// This function allocates memory for a new node, takes user input, and returns
-// the node
-struct slinklist *getnode() {
-  struct slinklist *newnode; // Declare a pointer for the new node
-
-  // Allocate memory for the new node using malloc
-  // sizeof(struct slinklist) ensures correct amount of memory is allocated
-  newnode = (struct slinklist *)malloc(sizeof(struct slinklist));
-
-  // Prompt user to enter data for the node
-  printf("\nEnter the data: ");
-
-  // Read the integer data from user and store it in the data field
-  // &newnode->data gives the address of the data field
+// Function to create and allocate memory for a new node
+struct dlinklist *getnode() {
+  struct dlinklist *newnode;
+  // Allocate memory
+  newnode = (struct dlinklist *)malloc(sizeof(struct dlinklist));
+  printf("\n Enter data: ");
+  // Input data
   scanf("%d", &newnode->data);
-
-  // Initialize the next pointer to NULL
-  // This makes the new node the last node (no successor)
-  newnode->next = NULL;
-
-  // Return the address of the newly created node
+  newnode->next = NULL; // Initialize next to NULL
   return newnode;
 }
 
-// Function to traverse and display all elements in the linked list
+// Function to traverse and print the contents of the list
 void traverse() {
-  struct slinklist *temp; // Temporary pointer for traversal
+  struct dlinklist *temp;
+  temp = start;
+  printf("\n The contents of List (Left to Right): \n\n");
 
-  // Note: Bug in original code - temp should be initialized to start
-  // temp = start; should be added here
-
-  // Loop continues until we reach the end of the list (NULL)
-  while (temp != NULL) {
-    // Print the data of the current node
-    printf("%d ", temp->data);
-
-    // Move to the next node in the list
-    temp = temp->next;
+  // Check if list is empty
+  if (start == NULL) {
+    printf("\n Empty List");
+    return;
+  } else {
+    // Iterate through the list until end
+    while (temp != NULL) {
+      printf(" %d-->", temp->data);
+      temp = temp->next; // Move to next node
+    }
   }
+  printf("NULL");
+}
+
+// Function to create a list with n nodes initially
+void createlist(int n) {
+  int i;
+  struct dlinklist *newnode;
+  struct dlinklist *temp;
+
+  for (i = 0; i < n; i++) {
+    newnode = getnode(); // Create a new node
+
+    if (start == NULL) {
+      // If list is empty, make new node the start
+      start = newnode;
+    } else {
+      // Otherwise, traverse to the end and append
+      temp = start;
+      while (temp->next != NULL)
+        temp = temp->next;
+      temp->next = newnode;
+    }
+  }
+}
+
+// Function to insert a node at the end of the existing list
+void insert_at_end() {
+  struct dlinklist *newnode, *temp;
+  printf(" This is for new struct dlinklist at the end");
+  newnode = getnode(); // Create new node to insert
+
+  if (start == NULL) {
+    // If list is empty, new node becomes start
+    start = newnode;
+  } else {
+    // Traverse to the last node
+    temp = start;
+    while (temp->next != NULL)
+      temp = temp->next;
+    // Point the last node's next to the new node
+    temp->next = newnode;
+  }
+}
+
+int main() {
+  int ch, n;
+
+  // Check if list is not already created
+  if (start == NULL) {
+    printf("\n Number of nodes you want to create: ");
+    scanf("%d", &n);
+    createlist(n); // Create initial list
+    printf("\n List created..\n");
+    insert_at_end(); // Insert an additional node at the end
+  } else {
+    printf("\n List is already created..");
+  }
+  traverse(); // Display the list
+  return 0;
 }
